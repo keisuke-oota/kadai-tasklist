65c099fe11169fb847ecb5593ae6fc174d8491df
keisuke.oota <o.keisuke0530@icloud.com>
2020-06-20 08:19:42 +0000

    tasks controllerr edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,10 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  
   def index
     if logged_in?
-      @tasks = Task.all
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
     end
   end
 
@@ -14,14 +17,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に追加されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task が追加されませんでした'
-      render :new
+      render 'tasks/index'
     end
   end
 
@@ -33,7 +36,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
     
     if @task.update(task_params)
-      flash[:success] = 'Taskは正常に追加されました'
+      flash[:success] = 'Taskは正常に更新されました'
       redirect_to @task
     else
       flash.now[:danger] = 'Taskは更新されませんでした'
@@ -56,4 +59,10 @@ class TasksController < ApplicationController
     params.require(:task).permit(:content, :status)
   end
   
+  def correct_user
+    @tassk = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
