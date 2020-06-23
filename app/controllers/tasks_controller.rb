a4b823889650c47e873ad4007e3facf81a86ae2a
keisuke.oota <o.keisuke0530@icloud.com>
2020-06-23 12:06:02 +0000

    tasksc edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :create, :destroy]
+  before_action :correct_user, only: [:edit, :update, :create, :destroy, :show]
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
