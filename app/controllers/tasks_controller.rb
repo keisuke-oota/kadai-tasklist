276289efa6b586ff7a7d6ebb2e2fc5a2791b0709
keisuke.oota <o.keisuke0530@icloud.com>
2020-06-23 05:44:37 +0000

    edit tasksc

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :update, :create, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
@@ -59,9 +60,9 @@ class TasksController < ApplicationController
   end
   
   def correct_user
-    @tassk = current_user.tasks.find_by(id: params[:id])
-    unless @task
-      redirect_to root_url
-    end
+    @task = current_user.tasks.find_by(id: params[:id])
+      unless @task
+        redirect_to root_url
+      end
   end
 end
