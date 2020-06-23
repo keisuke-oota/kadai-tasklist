0e41d1a7115ab569b03c723403c5d8c044e70c80
keisuke.oota <o.keisuke0530@icloud.com>
2020-06-23 12:52:20 +0000

    tasks c create edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,13 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:edit, :update, :create, :destroy, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy, :show]
   
   def index
     @tasks = current_user.tasks.order(id: :desc).page(params[:page])
   end
 
   def show
-    @task = Task.find(params[:id])
+ 
   end
 
   def new
@@ -29,11 +29,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+
   end
 
   def update
-    @task = Task.find(params[:id])
+ 
     
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
@@ -45,7 +45,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+
     @task.destroy
     
     flash[:success] = 'Task は正常に削除されました'
