3a5766b4dea02b34bcc1d46b673b6cf42430a924
keisuke.oota <o.keisuke0530@icloud.com>
2020-06-25 04:18:38 +0000

    tasks  controller def create render

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,7 +24,7 @@ class TasksController < ApplicationController
     else
       @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'Task が追加されませんでした'
-      render :index
+      render :new
     end
   end
 
